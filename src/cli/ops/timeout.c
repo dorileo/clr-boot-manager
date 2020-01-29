dorileo/clr-boot-manager

commit 512323d975a587a4d07be422be8211b1adcb31dc

timeout/update: don't allocate/free manager twice
Avoid parsing flags, allocating BootManager twice when calling update
from time out command.

Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

src/cli/ops/timeout.c
@@ -93,7 +93,7 @@ bool cbm_command_set_timeout(int argc, char **argv)
                 fprintf(stdout, "New timeout value is: %d\n", n_val);
         }
 
-        return cbm_command_update(argc, argv);
+        return cbm_command_update_do(manager);
 }
 
 bool cbm_command_get_timeout(int argc, char **argv)

src/cli/ops/update.c
@@ -20,6 +20,7 @@
 #include "cli.h"
 #include "log.h"
 #include "nica/files.h"
+#include "update.h"
 
 bool cbm_command_update(int argc, char **argv)
 {
@@ -40,10 +41,19 @@ bool cbm_command_update(int argc, char **argv)
 
         boot_manager_set_update_efi_vars(manager, update_efi_vars);
         
+        return cbm_command_update_do(manager);
+}
+
+bool cbm_command_update_do(BootManager *manager)
+{
+        autofree(char) *root = NULL;
+        bool forced_image = false;
+
         if (!boot_manager_detect_kernel_dir(root)) {
                 fprintf(stderr, "No kernels detected on system to update\n");
                 return true;
         }
+
         if (root) {
                 autofree(char) *realp = NULL;
 

src/cli/ops/update.h
@@ -11,9 +11,11 @@
 
 #pragma once
 
+#include "bootman.h"
 #include "cli.h"
 
 bool cbm_command_update(int argc, char **argv);
+bool cbm_command_update_do(BootManager *manager);
 
 /*
  * Editor modelines  -  https://www.wireshark.org/tools/modelines.html

commit f96852c250c007a2651a93d2215b5d5964376ee7

fix regression introduced by 512323d
With the commit 512323d we started ignoring the --path flag therefore breaking image
mode use cases. This patch brings back --path flag.

Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

src/cli/ops/timeout.c
@@ -93,7 +93,7 @@ bool cbm_command_set_timeout(int argc, char **argv)
                 fprintf(stdout, "New timeout value is: %d\n", n_val);
         }
 
-        return cbm_command_update_do(manager);
+        return cbm_command_update_do(manager, root, false);
 }
 
 bool cbm_command_get_timeout(int argc, char **argv)

src/cli/ops/update.c
@@ -41,14 +41,11 @@ bool cbm_command_update(int argc, char **argv)
 
         boot_manager_set_update_efi_vars(manager, update_efi_vars);
         
-        return cbm_command_update_do(manager);
+        return cbm_command_update_do(manager, root, forced_image);
 }
 
-bool cbm_command_update_do(BootManager *manager)
+bool cbm_command_update_do(BootManager *manager, char *root, bool forced_image)
 {
-        autofree(char) *root = NULL;
-        bool forced_image = false;
-
         if (!boot_manager_detect_kernel_dir(root)) {
                 fprintf(stderr, "No kernels detected on system to update\n");
                 return true;

src/cli/ops/update.h
@@ -15,7 +15,7 @@
 #include "cli.h"
 
 bool cbm_command_update(int argc, char **argv);
-bool cbm_command_update_do(BootManager *manager);
+bool cbm_command_update_do(BootManager *manager, char *root, bool forced_image);
 
 /*
  * Editor modelines  -  https://www.wireshark.org/tools/modelines.html